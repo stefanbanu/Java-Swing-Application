58e13010b47fb3181c92088f1b40135b105e191e
stefanbanu <stefanbanu1@gmail.com>
2014-12-26 18:34:45 +0200

    Commit the Master.

---
+++ JavaSwingApp1/src/com/stefanbanu/app/MainClass.java
@@ -0,0 +1,21 @@
+package com.stefanbanu.app;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame frame = new JFrame("Hello World");
+				frame.setSize(600, 500);
+				frame.setLocationRelativeTo(null);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});	
+	}
+	
+}
